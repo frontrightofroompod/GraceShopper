296a1ace93929b816f544ca25b0a96c236c8cae9
Grant Weiss <grant.sweiss@gmail.com>
2019-02-28 16:03:12 -0600

    finish single user page / reviews

--- client/routes.js
+++ client/routes.js
@@ -11,7 +11,8 @@ import {
   ConnectedSingleBeer,
   ConnectedAddBeer,
   EditBeerForm,
-  ConnectedNewReview
+  ConnectedNewReview,
+  ConnectedSingleUser
 } from './components'
 
 import {me} from './store'
@@ -34,7 +35,6 @@ class Routes extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route exact path="/beers" component={ConnectedAllBeers} />
-        <Route exact path="/users" component={ConnectedAllUsers} />
         <Route exact path="/beers/:beerId" component={ConnectedSingleBeer} />
         {isLoggedIn && (
           <Switch>
@@ -45,8 +45,10 @@ class Routes extends Component {
               path="/beers/:beerId/review"
               component={ConnectedNewReview}
             />
+            <Route exact path="/users/:id" component={ConnectedSingleUser} />
             {isAdmin && (
               <Switch>
+                <Route exact path="/users" component={ConnectedAllUsers} />
                 <Route path="/beers/:beerId/edit" component={EditBeerForm} />
                 <Route path="/addBeer" component={ConnectedAddBeer} />
                 {/* Routes placed here are only available after logging in */}
--- script/seed.js
+++ script/seed.js
@@ -15,7 +15,14 @@ async function seed() {
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123', userType: 'admin'}),
+    User.create({
+      firstName: 'Cody',
+      lastName: 'grrrrr',
+      phoneNumber: '123445566',
+      email: 'cody@email.com',
+      password: '123',
+      userType: 'admin'
+    }),
     User.create({email: 'murphy@email.com', password: '123'}),
     User.create({email: 'test@email.com', password: '123'})
   ])
