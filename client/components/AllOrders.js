6f022349f686290716faa182233f79ce7ca5c2fe
Kyle Lee <Kylecrlee95@gmail.com>
2019-03-01 15:04:37 -0600

    implemented usertype views for all orders and single order. admins have mark order as completed/processing buttons on single order, and show all orders/pending orders on all orders view

--- client/components/AllOrders.js
+++ client/components/AllOrders.js
@@ -16,71 +16,132 @@ class AllOrders extends Component {
   }
 
   render() {
-    const {allOrders, onShowPendingOrders, fetchOrdersFromServer} = this.props
-    return (
-      <div>
-        <Container>
-          <Button onClick={onShowPendingOrders}> Show Pending Orders </Button>
-          <br />
-          <br />
-          <Button onClick={fetchOrdersFromServer}> All Orders </Button>
-          <br />
-          <br />
-          <Row>
-            {allOrders && allOrders.length !== 0 ? (
-              allOrders.map(order => (
-                <Col key={order.id} xs={12} sm={6} md={4} lg={3}>
-                  <Link to={`/orders/${order.id}`}>
-                    <Card>
-                      <Card.Body>
-                        <Card.Text>Order ID #: {order.id}</Card.Text>
-                        <Card.Text>Status: {order.status}</Card.Text>
-                        <Card.Text>Shipping Address:</Card.Text>
-                        <ul className="shippingInfo">
-                          <li>
-                            {' '}
-                            <strong>City:</strong> {order.city}
-                          </li>
-                          <li>
-                            {' '}
-                            <strong>Street: </strong> {order.streetAddress}
-                          </li>
-                          <li>
-                            {' '}
-                            <strong>State: </strong> {order.state}
-                          </li>
-                          <li>
-                            <strong>Country: </strong> {order.country}
-                          </li>
-                          <li>
-                            <strong>Zip: </strong> {order.zipCode}
-                          </li>
-                          <li>
-                            <strong>Phone: </strong>
-                            {order.phoneNumber}
-                          </li>
-                        </ul>
-                      </Card.Body>
-                    </Card>
-                  </Link>
-                </Col>
-              ))
-            ) : (
-              <div>
-                {' '}
-                <h3>'No Orders!'</h3>
-              </div>
-            )}
-          </Row>
-        </Container>
-      </div>
-    )
+    const {
+      allOrders,
+      onShowPendingOrders,
+      fetchOrdersFromServer,
+      user
+    } = this.props
+    if (user.userType === 'admin') {
+      return (
+        <div>
+          <Container>
+            <Button onClick={onShowPendingOrders}> Show Pending Orders </Button>
+            <br />
+            <br />
+            <Button onClick={fetchOrdersFromServer}> All Orders </Button>
+            <br />
+            <br />
+            <Row>
+              {allOrders && allOrders.length !== 0 ? (
+                allOrders.map(order => (
+                  <Col key={order.id} xs={12} sm={6} md={4} lg={3}>
+                    <Link to={`/orders/${order.id}`}>
+                      <Card>
+                        <Card.Body>
+                          <Card.Text>Order ID #: {order.id}</Card.Text>
+                          <Card.Text>Status: {order.status}</Card.Text>
+                          <Card.Text>Shipping Address:</Card.Text>
+                          <ul className="shippingInfo">
+                            <li>
+                              {' '}
+                              <strong>City:</strong> {order.city}
+                            </li>
+                            <li>
+                              {' '}
+                              <strong>Street: </strong> {order.streetAddress}
+                            </li>
+                            <li>
+                              {' '}
+                              <strong>State: </strong> {order.state}
+                            </li>
+                            <li>
+                              <strong>Country: </strong> {order.country}
+                            </li>
+                            <li>
+                              <strong>Zip: </strong> {order.zipCode}
+                            </li>
+                            <li>
+                              <strong>Phone: </strong>
+                              {order.phoneNumber}
+                            </li>
+                          </ul>
+                        </Card.Body>
+                      </Card>
+                    </Link>
+                  </Col>
+                ))
+              ) : (
+                <div>
+                  {' '}
+                  <h3>'No Orders!'</h3>
+                </div>
+              )}
+            </Row>
+          </Container>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Container>
+            <Row>
+              {allOrders && allOrders.length !== 0 ? (
+                allOrders.map(order => (
+                  <Col key={order.id} xs={12} sm={6} md={4} lg={3}>
+                    <Link to={`/orders/${order.id}`}>
+                      <Card>
+                        <Card.Body>
+                          <Card.Text>Order ID #: {order.id}</Card.Text>
+                          <Card.Text>Status: {order.status}</Card.Text>
+                          <Card.Text>Shipping Address:</Card.Text>
+                          <ul className="shippingInfo">
+                            <li>
+                              {' '}
+                              <strong>City:</strong> {order.city}
+                            </li>
+                            <li>
+                              {' '}
+                              <strong>Street: </strong> {order.streetAddress}
+                            </li>
+                            <li>
+                              {' '}
+                              <strong>State: </strong> {order.state}
+                            </li>
+                            <li>
+                              <strong>Country: </strong> {order.country}
+                            </li>
+                            <li>
+                              <strong>Zip: </strong> {order.zipCode}
+                            </li>
+                            <li>
+                              <strong>Phone: </strong>
+                              {order.phoneNumber}
+                            </li>
+                          </ul>
+                        </Card.Body>
+                      </Card>
+                    </Link>
+                  </Col>
+                ))
+              ) : (
+                <div>
+                  {' '}
+                  <h3>'No Orders!'</h3>
+                </div>
+              )}
+            </Row>
+          </Container>
+        </div>
+      )
+    }
   }
 }
 
 const mapStateToProps = state => {
   return {
-    allOrders: state.allOrders
+    allOrders: state.allOrders,
+    user: state.user
   }
 }
 
--- client/components/SingleOrder.js
+++ client/components/SingleOrder.js
@@ -16,68 +16,104 @@ class SingleOrder extends React.Component {
     const {
       singleOrder,
       onMarkOneOrderAsProcessing,
-      onMarkOneOrderAsCompleted
+      onMarkOneOrderAsCompleted,
+      user
     } = this.props
-
-    return (
-      <div>
-        <Container>
-          <Row>
-            <Col xs={12} sm={4}>
-              <Card>
-                <Card.Body>
-                  <Card.Text>
-                    ID: {singleOrder.id}
-                    <br />
-                    Status: {singleOrder.status}
-                    <br />
-                    Phone Number: {singleOrder.phoneNumber}
-                    <br />
-                    Street Address: {singleOrder.streetAddress}
-                    <br />
-                    Zip: {singleOrder.city}
-                    <br />
-                    Zip: {singleOrder.zipCode}
-                    <br />
-                    Phone Number: {singleOrder.phoneNumber}
-                    <br />
-                    State: {singleOrder.state}
-                    <br />
-                  </Card.Text>
-                </Card.Body>
-                <Button
-                  onClick={() =>
-                    onMarkOneOrderAsProcessing({
-                      id: singleOrder.id,
-                      status: 'processing'
-                    })
-                  }
-                >
-                  Mark as Processing
-                </Button>
-                <br />
-                <Button
-                  onClick={() =>
-                    onMarkOneOrderAsCompleted({
-                      id: singleOrder.id,
-                      status: 'completed'
-                    })
-                  }
-                >
-                  Mark as Completed
-                </Button>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    )
+    if (user.userType === 'admin') {
+      return (
+        <div>
+          <Container>
+            <Row>
+              <Col xs={12} sm={4}>
+                <Card>
+                  <Card.Body>
+                    <Card.Text>
+                      ID: {singleOrder.id}
+                      <br />
+                      Status: {singleOrder.status}
+                      <br />
+                      Phone Number: {singleOrder.phoneNumber}
+                      <br />
+                      Street Address: {singleOrder.streetAddress}
+                      <br />
+                      Zip: {singleOrder.city}
+                      <br />
+                      Zip: {singleOrder.zipCode}
+                      <br />
+                      Phone Number: {singleOrder.phoneNumber}
+                      <br />
+                      State: {singleOrder.state}
+                      <br />
+                    </Card.Text>
+                  </Card.Body>
+                  <Button
+                    onClick={() =>
+                      onMarkOneOrderAsProcessing({
+                        id: singleOrder.id,
+                        status: 'processing'
+                      })
+                    }
+                  >
+                    Mark as Processing
+                  </Button>
+                  <br />
+                  <Button
+                    onClick={() =>
+                      onMarkOneOrderAsCompleted({
+                        id: singleOrder.id,
+                        status: 'completed'
+                      })
+                    }
+                  >
+                    Mark as Completed
+                  </Button>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Container>
+            <Row>
+              <Col xs={12} sm={4}>
+                <Card>
+                  <Card.Body>
+                    <Card.Text>
+                      ID: {singleOrder.id}
+                      <br />
+                      Status: {singleOrder.status}
+                      <br />
+                      Phone Number: {singleOrder.phoneNumber}
+                      <br />
+                      Street Address: {singleOrder.streetAddress}
+                      <br />
+                      Zip: {singleOrder.city}
+                      <br />
+                      Zip: {singleOrder.zipCode}
+                      <br />
+                      Phone Number: {singleOrder.phoneNumber}
+                      <br />
+                      State: {singleOrder.state}
+                      <br />
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      )
+    }
   }
 }
 
 const mapStateToProps = state => {
   return {
-    singleOrder: state.singleOrder
+    singleOrder: state.singleOrder,
+    user: state.user
   }
 }
 
